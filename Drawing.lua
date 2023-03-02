e8707d700e0cf7237053f34ddbb0aa23ad49e1df
Make hacks for iOS <67075721+frayray909090@users.noreply.github.com>
2023-03-01 18:03:42 -0800

    Create Drawing.lua

---
+++ Drawing.lua
@@ -0,0 +1,14 @@
+local Drawing = {}
+
+function Draw(name,trans,thick)
+  local Line = Drawing.new(""..name.."")
+  Line.Visible = true
+  Line.From = Vector2.new(0, 0)
+  Line.To = Vector2.new(200, 200)
+  Line.Color = Color3.fromRGB(255, 255, 255)
+  Line.Thickness = thick
+  Line.Transparency = trans
+  LIne.ZIndex = 1
+end
+
+return Drawing
